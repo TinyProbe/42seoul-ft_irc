a6487485823004406b0f82f1d3a3e2f2d2cab3d8
migo <migo@student.42.seoul.kr>
2023-10-25 19:15:35 +0900

    client4

--- source/Server.cpp
+++ source/Server.cpp
@@ -43,21 +43,21 @@ void Server::standby() {
   int flag = 1;
 
   if ((Server_socket = socket(PF_INET, SOCK_STREAM, 0)) == -1)
-		throw std::runtime_error(std::string("socket() error\n") + 
+    throw std::runtime_error(std::string("socket() error\n") + 
                              std::string(strerror(errno)));
-	setsockopt(Server_socket, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag)); //프로그램이 종료되어도 8080port가 해제되지 않는 현상 없애기 위해 필요
-	memset(&server_addr, 0, sizeof(server_addr));
-	server_addr.sin_family      = AF_INET;
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_addr.sin_port        = htons(Port_number_);
-	fcntl(Server_socket, F_SETFL, O_NONBLOCK);
-	if (bind(Server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
-		throw std::runtime_error(std::string("bind() error\n") + 
+  setsockopt(Server_socket, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag)); //프로그램이 종료되어도 8080port가 해제되지 않는 현상 없애기 위해 필요
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family      = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port        = htons(Port_number_);
+  fcntl(Server_socket, F_SETFL, O_NONBLOCK);
+  if (bind(Server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
+    throw std::runtime_error(std::string("bind() error\n") + 
                              std::string(strerror(errno)));
-	if (listen(Server_socket, MAX_BACKLOG) == -1)
-		throw std::runtime_error(std::string("listen() error\n") + 
+  if (listen(Server_socket, MAX_BACKLOG) == -1)
+    throw std::runtime_error(std::string("listen() error\n") + 
                              std::string(strerror(errno)));
-	server_socket_ = Server_socket;
+  server_socket_ = Server_socket;
 }
 
 void Server::preProcess() {
@@ -65,18 +65,18 @@ void Server::preProcess() {
 }
 
 void Server::disconnect(int socket) {
-	nick_to_socket_.erase(connections_[socket].getNickname());
-	connections_.erase(socket);
-	close (socket);
+  nick_to_socket_.erase(connections_[socket].getNickname());
+  connections_.erase(socket);
+  close (socket);
 }
 
 int Server::accept() {
-	int socket = accept(server_socket_, NULL, NULL);
-	if (socket == -1) {
-			throw std::runtime_error(std::string("accept() error\n") + 
-                               std::string(strerror(errno)));
-	}
-	fcntl(socket, F_SETFL, O_NONBLOCK);
-	connections_[socket];
-	return socket;
+  int socket = accept(server_socket_, NULL, NULL);
+  if (socket == -1) {
+    throw std::runtime_error(std::string("accept() error\n") + 
+                             std::string(strerror(errno)));
+  }
+  fcntl(socket, F_SETFL, O_NONBLOCK);
+  connections_[socket];
+  return socket;
 }
